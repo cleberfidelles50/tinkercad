3c764091b17258de9dcf5f2712c044f195570139
Cleber <cleberfigueiredofidelles101@gmail.com>
2024-07-16 16:32:29 -0300

    segunda versão

--- gitTinkercad.ino
+++ gitTinkercad.ino
@@ -1,14 +1,13 @@
+#define led 1
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
+  pinMode(led, OUTPUT);
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
-  while(true){
-    Serial.println("Já estamos na 7º semana");
-  }
+  digitalWrite(led, true);
 
 }
